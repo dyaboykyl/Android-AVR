5947e3ffa8bf2985912422a4853f155678fe6d05
Zack <zack.brandes@gmail.com>
2014-08-23 18:14:06 -0400

    All code.

---
+++ MainActivity.java
@@ -0,0 +1,236 @@
+package com.example.simplesocket;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+import java.util.Scanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * This application sets up a server side socket on port 38300.  The application sends color intensity
+ * values to the client to modulate an RGB LED. 
+ * 
+ * This code was heavily borrowed from this website: http://www.florescu.org/archives/2010/10/15/android-usb-connection-to-pc/comment-page-1/
+ * Thank you Alex Florescu.
+ * s
+ * @author Zack 
+ *
+ */
+
+public class MainActivity extends Activity implements OnClickListener, OnSeekBarChangeListener {
+
+	public static final int TIMEOUT = 6;
+
+	private String connectionStatus = null;
+	private String socketData = null;
+	private Handler mHandler = null;
+	ServerSocket server = null;
+	boolean running = false;
+
+	public static boolean connected;
+	public static Scanner socketIn;
+	public static BufferedOutputStream socketOut;
+
+	int red;
+	int blue;
+	int green;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		// Set up click listeners for the buttons
+		View connectButton = findViewById(R.id.connect_button);
+		connectButton.setOnClickListener(this);
+		View closeBtn = findViewById(R.id.close);
+		closeBtn.setOnClickListener(this);
+
+		//Set up listeners for the seekbars
+		SeekBar seekBar = (SeekBar) findViewById(R.id.red_bar);
+		seekBar.setOnSeekBarChangeListener(this);
+		TextView tv = (TextView) findViewById(R.id.red_text);
+		tv.setText("Red Intensity: "+seekBar.getProgress());
+		
+		seekBar = (SeekBar)findViewById(R.id.blue_bar);
+		seekBar.setOnSeekBarChangeListener(this);
+		tv = (TextView) findViewById(R.id.blue_text);
+		tv.setText("Blue Intensity: "+seekBar.getProgress());
+		
+		seekBar = (SeekBar)findViewById(R.id.green_bar);
+		seekBar.setOnSeekBarChangeListener(this);
+		tv = (TextView) findViewById(R.id.green_text);
+		tv.setText("Green Intensity: "+seekBar.getProgress());
+
+		mHandler = new Handler();
+	}
+
+	/**
+	 * Button click implementations
+	 */
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.connect_button:
+			// initialize server socket in a new separate thread
+			new Thread(initializeConnection).start();
+			String msg = "Attempting to connect...";
+			Toast.makeText(this, msg, msg.length()).show();
+			break;		
+		case R.id.close:
+			//Close connection
+			if (socketOut != null) {
+				try {
+					socketOut.write('q');				
+					socketOut.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}		
+			}
+			running = false;
+			break;
+		default:
+			break;
+		}
+	}
+
+	/**
+	 * SeekBar listener implementation
+	 */
+	@Override
+	public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+		//Color character
+		char color = 'x';
+		//Color intensity
+		int value = 1;
+		//Color TextView
+		TextView textView = null;
+		String text = "";
+		switch(seekBar.getId()){
+		case R.id.red_bar:
+			color = 'r';
+			textView = (TextView) findViewById(R.id.red_text);
+			text = "Red Intensity: ";
+			break;
+
+		case R.id.blue_bar:
+			color = 'b';
+			textView = (TextView) findViewById(R.id.blue_text);
+			text = "Blue Intensity: ";
+			break;
+
+		case R.id.green_bar:
+			color = 'g';
+			textView = (TextView) findViewById(R.id.green_text);
+			text = "Green Intensity: ";
+			break;
+
+		default:
+			break;
+		}
+		if(color != 'x'){
+			value = seekBar.getProgress() + 1;  //Add 1 so that a 0 is never sent
+			if(textView != null) textView.setText(text+value);
+			if (socketOut != null) {
+				try {					
+					socketOut.write((byte)color);
+					socketOut.write((byte)value);
+					socketOut.flush();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}	
+		}
+
+	}
+
+	@Override
+	public void onStartTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStopTrackingTouch(SeekBar seekBar) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private Runnable initializeConnection = new Thread() {
+		public void run() {
+
+			Socket client = null;
+			running = true;
+			// initialize server socket
+			try {
+				server = new ServerSocket(38300);
+				server.setSoTimeout(TIMEOUT * 1000);
+
+				// attempt to accept a connection
+				client = server.accept();
+				socketIn = new Scanner(client.getInputStream());
+				socketOut = new BufferedOutputStream(client.getOutputStream());
+			} catch (SocketTimeoutException e) {
+				// print out TIMEOUT
+				connectionStatus = "Connection has timed out! Please try again";
+				mHandler.post(showConnectionStatus);
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				// close the server socket
+				try {
+					if (server != null)
+						server.close();
+				} catch (IOException ec) {
+					ec.printStackTrace();
+				}
+			}
+
+			if (client != null) {
+				connected = true;
+				// print out success
+				connectionStatus = "Connection was succesful!";
+				mHandler.post(showConnectionStatus);
+				while (socketIn.hasNext() && running) {
+					socketData = socketIn.next();
+				}				
+
+				try {
+					connectionStatus = "Connection closed";
+					mHandler.post(showConnectionStatus);
+					client.close();
+					server.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+
+			}
+
+		}
+	};
+
+	/**
+	 * Pops up a "toast" to indicate the connection status
+	 */
+	private Runnable showConnectionStatus = new Runnable() {
+		public void run() {
+			Toast.makeText(getBaseContext(), connectionStatus,
+					Toast.LENGTH_SHORT).show();
+		}
+	};
+
+
+
+
+
+}
--- README.md
+++ README.md
@@ -1,4 +1,56 @@
 Android-AVR
 ===========
 
-Android app to control rgb led on atmega328
+Zack Brandes
+zack.brandes@gmail.com
+8/23/2014
+
+https://www.youtube.com/watch?v=yCX6QGX9yBk&feature=youtu.be
+
+This repository contains three elements: 
+
+1.		Android code to set up a server socket and send RGB values to the client.
+2.		A python script that sets up the client side socket on the computer to read in the RGB values
+			and send them to an AVR micro-controller via USART serial communication.
+3.		A C program written for an atmega328 that receives USART data in order to modulate PWM duty cycles and control and RGB LED.
+
+IT IS IMPORTANT THAT YOU READ THESE INSTRUCTIONS CAREFULLY
+
+NOTE: The Android server socket code was taken heavily from Alec Florescu's website. Thank you Alex.
+http://www.florescu.org/archives/2010/10/15/android-usb-connection-to-pc/comment-page-1/
+
+RGB LED:
+The AVR code works for a common anode RGB LED wired to an atmega328 as following:
+	Red LED to PD5
+	Blue LED to PD6
+	Green LED to PB5
+Refer to the atmega328 pinout for clarification.
+If you are using the a common cathode RGB LED, then you will need to edit the while loop of the AVR code, 
+	otherwise you will get opposite results.
+
+ANDROID APP:
+1. Create a new android app and use the MainActivity.java and activity_main.xml files in the repo.
+2. Update the android manifest file with 
+
+    <uses-permission android:name="android.permission.INTERNET"/> under the <manifest> tag.
+	
+3. Upload the app to your device.
+
+AVR Code:
+The code was written and uploaded using AtmelStudio, however it should work in the Arduino IDE.
+1. Upload the code.
+
+PYTHON SCRIPT
+1. Before the script can be run, the android device must be connected to the computer and USB debugging mode must be enabled.
+2. Open a cmd prompt and execute the following line
+
+	"PATH_TO_ADB"/adb.exe forward tcp:38300 tcp:38300
+	
+	This forwards a port on the computer to the device, allowing a socket communication to be set up on localhost.
+	If at any time your device becomes unplugged, the above line must be executed again.
+3. Make sure there are no open serial interfaces with the micro-controller/arduino.
+4. Open the Android app, hit connect, and then run the python script.  A message should pop up on the app if the connection was 
+	successful, or if the connection timed out.
+5.	You should now be able to modulate the RGB LED using the Android app.
+	
+
